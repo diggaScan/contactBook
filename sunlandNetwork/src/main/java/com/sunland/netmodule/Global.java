2f89351f32041d6890587c25af1b05c4f250d120
peitaoYe <peitaotom@gmail.com>
2018-12-28 09:11:10 +0800

    version 2.1

--- sunlandNetwork/src/main/java/com/sunland/netmodule/Global.java
+++ sunlandNetwork/src/main/java/com/sunland/netmodule/Global.java
@@ -5,11 +5,15 @@ package com.sunland.netmodule;
  */
 
 public class Global {
-    public final static String ip = "20.65.2.12";
     // 3576测试服务端口,映射9081
     // 3565正式服务端口，
     // 3579
-    public static String port = "3576";
+    public final static String ip = "10.10.100.6";
+    //    public final static String ip = "20.65.2.12";
+    //    public static String port = "3576";
+    //    public static String port = "52006";
+    public static String port = "52006";
+    //    public final static String postfix = "/hzydjw/services/IQueryOfJwtService";
     public final static String postfix = "/hzydjw/services/IHzydjwService";
 
 }
